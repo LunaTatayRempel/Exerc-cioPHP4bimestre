b9d3d2fea5f235ab666817ac5b7832e25114ca58
Luna <luna.rempel@etec.sp.gov.br>
2018-10-16 11:07:32 -0300

    primeiro commit

---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php
+
+function DB_Connect(){
+    $PDO = new PDO('mysql:host=' .  DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET, DB_USER, DB_PASS);
+    return $PDO;
+}
+
+function imageFile($img, $update){
+    
+    // Apagando imagem antiga caso estivesse dando update
+
+    // if(!empty($update)){
+    //     unlink( "img/$update");
+    // }
+
+    $extensao = strtolower(substr($img["name"], -4));
+    $novoNome = md5(time());
+    $diretorio = "img/";
+    
+    move_uploaded_file($img["tmp_name"], $diretorio.$novoNome);
+    
+    return $novoNome;
+}
+
+function dateConvert($date){
+    if(!strstr($date, "/")){
+
+        sscanf($date, "%d-%d-%d", $y, $m, $d);
+        return sprintf('%02d/%02d/%04d', $d, $m, $y);
+    }
+
+    return false;
+}
